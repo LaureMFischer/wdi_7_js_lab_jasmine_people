6f92cb601badc0e4ff17a568bb18c39b59e75627
Laure Fischer <laurefischer27@gmail.com>
2014-02-19 17:47:28 -0500

    all but last requirement satisfied

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -3,7 +3,8 @@ describe('Person', function(){
 
   beforeEach(function() {
     person_1 = new Person('Laure', 'Fischer');
-    person_2 = new Person('Anna', 'Tsykalova', {age: 28, friends: ["Laure", "Emilie", "Libby"], weight: 155, height: 68})
+    person_2 = new Person('Anna', 'Tsykalova', {age: 28, friends: [person_1], weight: 155, height: 68});
+    person_3 = new Person('Joe', 'Smith', {age: 40, weight: 170, height: 75});
   })
 
   it("should have a first name", function() {
@@ -18,7 +19,7 @@ describe('Person', function(){
     expect(person_2.age).toEqual(28);
     expect(person_2.weight).toEqual(155);
     expect(person_2.height).toEqual(68);
-    expect(person_2.friends).toEqual(["Laure","Emilie","Libby"]);
+    expect(person_2.friends).toEqual([person_1]);
   });
 
   it("should be possible to return a person's first and last name", function() {
@@ -29,5 +30,31 @@ describe('Person', function(){
   it("should be possible to return a person's weight in stone", function() {
     expect(person_2.weightInStone()).toEqual(11);
   });
+
+  it("should be possible to add a person to a person's friends", function() {
+    person_2.addFriend(person_3);
+    expect(person_2.friends).toEqual([person_1, person_3]);
+  });
+
+  it("should be possible to remove a person from a person's friends", function() {
+    person_2.removeFriend(person_1);
+    expect(person_2.friends).toEqual([]);
+  });
+
+  it("should be possible to greet people with nothing provided", function() {
+    expect(person_2.greetPeople()).toEqual("Hi Laure Fischer");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
--- src/Person.js
+++ src/Person.js
@@ -2,10 +2,10 @@ var Person = function(first, last, options) {
   options = options || {};
   this.firstName = first;
   this.lastName = last;
-  this.age = options.age;
-  this.weight = options.weight;
-  this.height = options.height;
-  this.friends = options.friends;
+  this.age = options.age || parseInt(Math.random()*(46-26));
+  this.weight = options.weight || parseInt(Math.random()*(101-61));
+  this.height = options.height || parseInt(Math.random()*(201-151));
+  this.friends = options.friends || [];
   // this.fullName = this.firstName + " " + this.lastName;
 };
 
@@ -17,4 +17,17 @@ Person.prototype.weightInStone = function() {
   return parseInt(this.weight / 14);
 };
 
+Person.prototype.addFriend = function(person) {
+  this.friends.push(person);
+};
 
+Person.prototype.removeFriend = function(person) {
+  var index = this.friends.indexOf(person);
+  this.friends.splice(index, 1);
+};
+
+Person.prototype.greetPeople = function() {
+  this.friends.forEach(fullName()) {
+    return "Hi " + person.fullName();
+  };
+}
